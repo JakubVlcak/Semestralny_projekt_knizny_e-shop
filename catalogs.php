391ca730a6579236a5defbd99777896a68a76102
JakubVlcak <jakub.vlcak888@gmail.com>
2023-04-25 20:49:35 +0200

    add catalog.php

---
+++ catalogs.php
@@ -0,0 +1,14 @@
+<?php
+include_once 'parts/html_header.php';
+?>
+
+<body>
+    <div class="container">
+        <?php
+        include_once 'parts/navigation.php';
+        ?>
+    </div>
+    <?php
+    include_once 'parts/footer.php';
+    ?>
+</body>
