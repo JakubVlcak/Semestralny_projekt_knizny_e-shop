7c9d204410ff70e24327fa9532d389e67dc2f2a1
JakubVlcak <jakub.vlcak888@gmail.com>
2023-04-25 22:48:20 +0200

    add books to index

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 include_once 'parts/html_header.php';
+include_once 'environmental_variables.php';
+include_once 'db_connection.php';
 ?>
 
 <body>
@@ -19,72 +21,47 @@ include_once 'parts/html_header.php';
                 <p>Shelf HTML template is provided by Tooplate. Please tell your friends about it. Thank you. Images are from Unsplash website. In tincidunt metus sed justo tincidunt sollicitudin. Curabitur magna tellus, condimentum vitae consectetur id, elementum sit amet erat.</p>
 
                 <div class="tm-gallery">
+                    <?php
+                    $sql = "SELECT * FROM books";
+                    $result = mysqli_query($conn, $sql);
+                    ?>
                     <div class="row">
-                        <figure class="col-lg-3 col-md-4 col-sm-6 col-12 tm-gallery-item">
-                            <a href="preview.html">
-                                <div class="tm-gallery-item-overlay">
-                                    <img src="img/image-01.jpg" alt="Image" class="img-fluid tm-img-center">
-                                </div>
+                        <?php
+                        if (mysqli_num_rows($result) > 0) {
+                        // Start output buffering
+                        ob_start();
 
-                                <p class="tm-figcaption">Nam vitae odio</p>
-                            </a>
-                        </figure>
-                        <figure class="col-lg-3 col-md-4 col-sm-6 col-12 tm-gallery-item">
-                            <a href="preview.html">
-                                <div class="tm-gallery-item-overlay">
-                                    <img src="img/image-02.jpg" alt="Image" class="img-fluid tm-img-center">
-                                </div>
-                                <p class="tm-figcaption">Integer ornare</p>
-                            </a>
-                        </figure>
-                        <figure class="col-lg-3 col-md-4 col-sm-6 col-12 tm-gallery-item">
-                            <a href="preview.html">
-                                <div class="tm-gallery-item-overlay">
-                                    <img src="img/image-03.jpg" alt="Image" class="img-fluid tm-img-center">
-                                </div>
-                                <p class="tm-figcaption">Cras non addugue</p>
-                            </a>
-                        </figure>
-                        <figure class="col-lg-3 col-md-4 col-sm-6 col-12 tm-gallery-item">
-                            <a href="preview.html">
-                                <div class="tm-gallery-item-overlay">
-                                    <img src="img/image-04.jpg" alt="Image" class="img-fluid tm-img-center">
-                                </div>
-                                <p class="tm-figcaption">Dolor lacus</p>
-                            </a>
-                        </figure>
-                        <figure class="col-lg-3 col-md-4 col-sm-6 col-12 tm-gallery-item">
-                            <a href="preview.html">
-                                <div class="tm-gallery-item-overlay">
-                                    <img src="img/image-05.jpg" alt="Image" class="img-fluid tm-img-center">
-                                </div>
-                                <p class="tm-figcaption">Quisque velit</p>
-                            </a>
-                        </figure>
-                        <figure class="col-lg-3 col-md-4 col-sm-6 col-12 tm-gallery-item">
-                            <a href="preview.html">
-                                <div class="tm-gallery-item-overlay">
-                                    <img src="img/image-06.jpg" alt="Image" class="img-fluid tm-img-center">
-                                </div>
-                                <p class="tm-figcaption">Suspendisse suscipit</p>
-                            </a>
-                        </figure>
-                        <figure class="col-lg-3 col-md-4 col-sm-6 col-12 tm-gallery-item">
-                            <a href="preview.html">
-                                <div class="tm-gallery-item-overlay">
-                                    <img src="img/image-07.jpg" alt="Image" class="img-fluid tm-img-center">
-                                </div>
-                                <p class="tm-figcaption">Facilisis justo</p>
-                            </a>
-                        </figure>
-                        <figure class="col-lg-3 col-md-4 col-sm-6 col-12 tm-gallery-item">
-                            <a href="preview.html">
-                                <div class="tm-gallery-item-overlay">
-                                    <img src="img/image-08.jpg" alt="Image" class="img-fluid tm-img-center">
-                                </div>
-                                <p class="tm-figcaption">Vivamus facilisis</p>
-                            </a>
-                        </figure>
+
+
+                        // Output each row of data
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            
+                        echo '<figure class="col-lg-3 col-md-4 col-sm-6 col-12 tm-gallery-item">';
+                        echo'<a href="preview.html">';
+                        echo'<div class="tm-gallery-item-overlay">';
+                        echo'<img src="' . $row["urlimage"] . '" alt="Image" class="img-fluid tm-img-center">';
+                                echo'</div>';
+
+                                echo'<p class="tm-figcaption">'. $row["title"] . '</p>';
+                            echo'</a>';
+                        echo '</figure>';
+
+                        }
+
+                
+                       
+
+                        // Get the output buffer contents and clear it
+                        $table = ob_get_clean();
+
+                        // Output the table to the browser
+                        echo $table;
+                        } else {
+                        echo "No results found.";
+                        }
+                        ?>
+
+                        
                     </div>
                 </div>
 
@@ -132,5 +109,8 @@ include_once 'parts/html_header.php';
     </script>
 
 </body>
+<?php
+include_once 'db_connection_close.php'
+?>
 
 </html>
