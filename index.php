a484cc46563eb3ddf2c63743e5923b37f7dc4f7d
JakubVlcak <jakub.vlcak888@gmail.com>
2023-04-26 21:09:10 +0200

    change of calling preview

--- index.php
+++ index.php
@@ -28,40 +28,39 @@ include_once 'db_connection.php';
                     <div class="row">
                         <?php
                         if (mysqli_num_rows($result) > 0) {
-                        // Start output buffering
-                        ob_start();
+                            // Start output buffering
+                            ob_start();
 
 
 
-                        // Output each row of data
-                        while ($row = mysqli_fetch_assoc($result)) {
-                            
-                        echo '<figure class="col-lg-3 col-md-4 col-sm-6 col-12 tm-gallery-item">';
-                        echo'<a href="preview.html">';
-                        echo'<div class="tm-gallery-item-overlay">';
-                        echo'<img src="' . $row["urlimage"] . '" alt="Image" class="img-fluid tm-img-center">';
-                                echo'</div>';
+                            // Output each row of data
+                            while ($row = mysqli_fetch_assoc($result)) {
 
-                                echo'<p class="tm-figcaption">'. $row["title"] . '</p>';
-                            echo'</a>';
-                        echo '</figure>';
+                                echo '<figure class="col-lg-3 col-md-4 col-sm-6 col-12 tm-gallery-item">';
+                                echo '<a href="preview.php?blog=' . $row['idbooks'] . '">';
+                                echo '<div class="tm-gallery-item-overlay">';
+                                echo '<img src="' . $row["urlimage"] . '" alt="Image" class="img-fluid tm-img-center">';
+                                echo '</div>';
 
-                        }
+                                echo '<p class="tm-figcaption">' . $row["title"] . '</p>';
+                                echo '</a>';
+                                echo '</figure>';
+                            }
 
-                
-                       
 
-                        // Get the output buffer contents and clear it
-                        $table = ob_get_clean();
 
-                        // Output the table to the browser
-                        echo $table;
+
+                            // Get the output buffer contents and clear it
+                            $table = ob_get_clean();
+
+                            // Output the table to the browser
+                            echo $table;
                         } else {
-                        echo "No results found.";
+                            echo "No results found.";
                         }
                         ?>
 
-                        
+
                     </div>
                 </div>
 
@@ -73,21 +72,6 @@ include_once 'db_connection.php';
                         <li class="nav-item"><a class="nav-link" href="#">4</a></li>
                     </ul>
                 </nav>
-            </section>
-
-            <section class="media tm-highlight tm-highlight-w-icon">
-
-                <div class="tm-highlight-icon">
-                    <i class="fa tm-fa-6x fa-meetup"></i>
-                </div>
-
-                <div class="media-body">
-                    <header>
-                        <h2>Need Help?</h2>
-                    </header>
-                    <p class="tm-margin-b">Curabitur magna tellus, condimentum vitae consectetur id, elementum sit amet erat. Phasellus arcu leo, sagittis fringilla nisi et, pulvinar vestibulum mi. Maecenas mollis ullamcorper est at dignissim.</p>
-                    <a href="" class="tm-white-bordered-btn">Live Chat</a>
-                </div>
             </section>
         </div>
         <?php
