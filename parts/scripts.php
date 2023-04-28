74d1ea363267ee85ccce1692d12536ca02555500
JakubVlcak <jakub.vlcak888@gmail.com>
2023-04-28 22:17:05 +0200

    add scripts

---
+++ parts/scripts.php
@@ -0,0 +1,4 @@
+<!-- load JS files -->
+<script src="js/jquery-1.11.3.min.js"></script> <!-- jQuery (https://jquery.com/download/) -->
+<script src="js/popper.min.js"></script> <!-- Popper (https://popper.js.org/) -->
+<script src="js/bootstrap.min.js"></script> <!-- Bootstrap (https://getbootstrap.com/) -->
