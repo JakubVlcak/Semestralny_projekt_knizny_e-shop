04e58d2712598dd24075316564955622e656cf35
JakubVlcak <jakub.vlcak888@gmail.com>
2023-04-28 23:39:05 +0200

    add names of inputs

--- log_in.php
+++ log_in.php
@@ -8,13 +8,13 @@ include_once 'parts/html_header.php';
     include_once 'parts/navigation.php';
     ?>
     <div class="text-center h-50 d-inline-block d-flex justify-content-center mt-9">
-      <form class="form-signin">
+      <form class="form-signin" action="authenticate.php" method="post">
       <h1 class="tm-site-name">Shelf</h1>
         <h1 class="h3 mb-3 font-weight-normal">Please log in</h1>
         <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
+        <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
         <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
+        <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required="">
         <div class="checkbox mb-3">
           <label>
             <input type="checkbox" value="remember-me"> Remember me
--- sign_in.php
+++ sign_in.php
@@ -8,13 +8,15 @@ include_once 'parts/html_header.php';
     include_once 'parts/navigation.php';
     ?>
     <div class="text-center h-50 d-inline-block d-flex justify-content-center mt-9">
-      <form class="form-signin">
+      <form class="form-signin" action="register.php" method="post">
       <h1 class="tm-site-name">Shelf</h1>
         <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+        <label for="name" class="sr-only">Name</label>
+        <input type="text" name="name" id="name" class="form-control" placeholder="Name" required="">
         <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
+        <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
         <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
+        <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required="">
         <div class="checkbox mb-3">
           <label>
             <input type="checkbox" value="remember-me"> Remember me
