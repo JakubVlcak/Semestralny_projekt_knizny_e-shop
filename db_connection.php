7af45cc5607b8e0ea7da9c8fc4f2341ae532b969
JakubVlcak <jakub.vlcak888@gmail.com>
2023-04-28 22:16:04 +0200

    delete old connection

--- db_connection.php
+++ db_connection.php
@@ -5,10 +5,6 @@ $username = getenv("DB_USER");
 $password = getenv("DB_PASS");
 $database = getenv("DB_NAME");
 
-// Create connection
-//$conn = mysqli_connect($host, $username, $password, $database);
-$conn = mysqli_connect($host, $username, $password, $database);
-
 try {
   $conn = new PDO("mysql:host=$host; dbname=$database", $username, $password);
 } catch (PDOException $exception) {
