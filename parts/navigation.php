0bc55d5fc2d33c3fccd19e7de94039284c7fa2ed
JakubVlcak <jakub.vlcak888@gmail.com>
2023-04-29 11:57:11 +0200

    change of navigation for authenticated user

--- parts/navigation.php
+++ parts/navigation.php
@@ -10,8 +10,20 @@
             <ul class="nav nav-fill tm-main-nav-ul">
                 <li class="nav-item"><a class="nav-link active" href="index.php">Home</a></li>
                 <li class="nav-item"><a class="nav-link" href="catalogs.php">Catalogs</a></li>
-                <li class="nav-item"><a class="nav-link" href="log_in.php">Log In</a></li>
-                <li class="nav-item"><a class="nav-link" href="sign_in.php">Sign In</a></li>
+                <?php
+                if (isset($_SESSION["authenticated"])) {
+                    if ($_SESSION["authenticated"] == 0) {
+                        echo '<li class="nav-item"><a class="nav-link" href="log_in.php">Log In</a></li>';
+                        echo '<li class="nav-item"><a class="nav-link" href="sign_in.php">Sign In</a></li>';
+                    }
+                    if ($_SESSION["authenticated"] == 1) {
+                        echo '<li class="nav-item"><a class="nav-link" href="log_out.php">Log Out</a></li>';
+                    }
+                } else {
+                    echo '<li class="nav-item"><a class="nav-link" href="log_in.php">Log In</a></li>';
+                    echo '<li class="nav-item"><a class="nav-link" href="sign_in.php">Sign In</a></li>';
+                }
+                ?>
                 <li class="nav-item"><a class="nav-link" href="cart.php">Shopping Cart &#128722</a></li>
             </ul>
         </div>
